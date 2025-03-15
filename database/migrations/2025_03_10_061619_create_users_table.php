a3d9fdcf78f58936b9b8f2bccae2b7319ce8f54f
skuze <skuzevanov@gmail.com>
2025-03-15 22:13:51 +0500

    Удалил лишний код в миграции.

--- database/migrations/2025_03_10_061619_create_users_table.php
+++ database/migrations/2025_03_10_061619_create_users_table.php
@@ -17,16 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-//            $table->unsignedBigInteger('role_id')->default(1);
             $table->unsignedBigInteger('role_id')->default(1)->nullable();
-
-//            $table->foreign('role_id')->references('id')->on('roles')->onDelete('set null');
-            $table->foreign('role_id')
-                ->references('id')
-                ->on('roles')
-                ->onDelete('set null');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('set null');
             $table->index('role_id');
-
             $table->rememberToken();
             $table->timestamps();
         });
