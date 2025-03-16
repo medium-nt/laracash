2e44d005457f0a7df4b69ee9e0e2de31bbb320fd
skuze <skuzevanov@gmail.com>
2025-03-16 08:00:34 +0500

    Добавил Envoy для локальной разработки.

---
+++ Envoy.blade.php
@@ -0,0 +1,39 @@
+@setup
+    $user = 'fh791290';
+    $server = 'fh791290.beget.tech';
+@endsetup
+
+@servers(['web' => [$user.'@'.$server]])
+
+@task('deploy', ['on' => 'web'])
+    set -e
+    echo "Deploying..."
+
+    git pull origin main
+    php8.3 artisan down
+
+    php8.3 composer.phar install --no-dev --optimize-autoloader
+    php8.3 artisan migrate --force
+
+    php8.3 artisan config:cache
+    php8.3 artisan route:cache
+    php8.3 artisan view:cache
+
+    php8.3 artisan up
+
+    echo "Done!"
+@endtask
+
+@task('pull', ['on' => 'web'])
+    set -e
+    echo "Deploying..."
+    git pull origin main
+    echo "Done!"
+@endtask
+
+@task('migrate', ['on' => 'web'])
+    set -e
+    echo "Deploying..."
+    php8.3 artisan migrate --force
+    echo "Done!"
+@endtask
