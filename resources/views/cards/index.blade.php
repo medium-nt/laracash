e29fe4ca85e107f899796543a45e2da68db10228
skuze <skuzevanov@gmail.com>
2025-11-24 08:14:53 +0500

    Поправил логику, чтобы не показывалась кнопка распознать если нет загруженного скрина. Поправил отображение таблицы карт на мобильных.

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -14,41 +14,43 @@
 
                 <a href="{{ route('cards.create') }}" class="btn btn-primary mb-3">Добавить Карту</a>
 
-                <table class="table table-hover table-bordered">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Номер карты</th>
-                            <th scope="col">Название банка</th>
-                            <th scope="col">Цвет</th>
-                            <th scope="col">Действия</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($cards as $card)
+                <div class="table-responsive">
+                    <table class="table table-hover table-bordered">
+                        <thead class="thead-dark">
                             <tr>
-                                <td>{{ $card->id }}</td>
-                                <td>{{ $card->number }}</td>
-                                <td>{{ $card->bank->title }}</td>
-                                <td><div style="width: 20px; height: 20px; background-color: {{$card->color}};"></div></td>
-                                <td>
-                                    <div class="btn-group" role="group">
-                                    <a href="{{ route('cards.edit', ['card' => $card->id]) }}" class="btn btn-primary mr-1">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                        <form action="{{ route('cards.destroy', ['card' => $card->id]) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger" onclick="return confirm('Вы уверены? Так же удалятся все кешбеки по этой карте.')">
-                                                <i class="fas fa-trash"></i>
-                                            </button>
-                                        </form>
-                                    </div>
-                                </td>
+                                <th scope="col">#</th>
+                                <th scope="col">Номер карты</th>
+                                <th scope="col">Название банка</th>
+                                <th scope="col">Цвет</th>
+                                <th scope="col">Действия</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($cards as $card)
+                                <tr>
+                                    <td>{{ $card->id }}</td>
+                                    <td>{{ $card->number }}</td>
+                                    <td>{{ $card->bank->title }}</td>
+                                    <td><div style="width: 20px; height: 20px; background-color: {{$card->color}};"></div></td>
+                                    <td>
+                                        <div class="btn-group" role="group">
+                                        <a href="{{ route('cards.edit', ['card' => $card->id]) }}" class="btn btn-primary mr-1">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                            <form action="{{ route('cards.destroy', ['card' => $card->id]) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger" onclick="return confirm('Вы уверены? Так же удалятся все кешбеки по этой карте.')">
+                                                    <i class="fas fa-trash"></i>
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
 
                 {{-- Pagination --}}
                 <x-pagination-component :collection="$cards" />
