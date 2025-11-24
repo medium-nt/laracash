ecd0346db26ee66b39727622e70ded736a2f83dc
skuze <skuzevanov@gmail.com>
2025-11-24 08:07:59 +0500

    Удалил повторяющийся card-body

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -10,11 +10,9 @@
 @section('content_body')
     <div class="col-md-12">
         <div class="card">
-            <div class="card-body">
 
-                @livewire('category-search-component')
+            @livewire('category-search-component')
 
-            </div>
         </div>
     </div>
 @stop
