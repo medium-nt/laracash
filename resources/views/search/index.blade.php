3610218733016bd5b965a7ec120c89587e103f8a
skuze <skuzevanov@gmail.com>
2025-11-28 18:38:51 +0500

    Добавил иконки и манифест для PWA.

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -12,6 +12,26 @@
 
     <meta name="mobile-web-app-capable" content="yes">
 
+    <!-- Apple Touch Icons -->
+    <link rel="apple-touch-icon" href="{{ asset('icons/icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('icons/icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('icons/icon-60x60.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('icons/icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('icons/icon-76x76.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('icons/icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('icons/icon-120x120.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('icons/icon-144x144.png') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('icons/icon-152x152.png') }}">
+    <link rel="apple-touch-icon" sizes="167x167" href="{{ asset('icons/icon-167x167.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('icons/icon-180x180.png') }}">
+
+    <!-- PWA Icons -->
+    <link rel="icon" sizes="192x192" href="{{ asset('icons/icon-192x192.png') }}">
+    <link rel="icon" sizes="512x512" href="{{ asset('icons/icon-512x512.png') }}">
+
+    <!-- PWA Manifest -->
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
+
     <title>Твой кешбек</title>
 
     <!-- Font Awesome Icons -->
